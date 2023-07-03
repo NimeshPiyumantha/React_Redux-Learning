77eb48bb800360e85954662eba7fa333cb94fb21
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-03 12:24:20 +0530

    Add Action

--- asyncActions.js
+++ asyncActions.js
@@ -11,6 +11,7 @@ const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
 const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
 const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
 
+
 const fetchUsersRequest = () => {
   return {
     type: FETCH_USERS_REQUEST,
