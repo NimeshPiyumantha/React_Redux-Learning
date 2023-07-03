54d028dd30cd02d0e0b231ab404de7c3e802fca3
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-03 11:26:13 +0530

    Add combineReducers

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const redux = require("redux");
-
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
-// Action Creator
 function buyCake() {
   return {
     type: BUY_CAKE,
@@ -12,11 +12,22 @@ function buyCake() {
   };
 }
 
-const initialState = {
+function buyIcecream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "Second redux action",
+  };
+}
+
+const initialCakeState = {
   numOfCakes: 10,
 };
 
-const reducer = (state = initialState, action) => {
+const initialIcecreamState = {
+  numOfIcecream: 20,
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -28,15 +39,36 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
-console.log("Initial state", store.getState()); //Initial state { numOfCakes: 10 }
+const icecreamReducer = (state = initialIcecreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIcecream: state.numOfIcecream - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// Combine the cake and ice cream reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  icecream: icecreamReducer,
+});
+
+const store = createStore(rootReducer);
+console.log("Initial state", store.getState()); //Initial state { cake: { numOfCakes: 10 }, icecream: { numOfIcecream: 20 } }
 
 const unsubscribe = store.subscribe(() =>
   console.log("Updated state", store.getState())
 );
 
-store.dispatch(buyCake()); //Updated state { numOfCakes: 9 }
-store.dispatch(buyCake()); //Updated state { numOfCakes: 8 }
-store.dispatch(buyCake()); //Updated state { numOfCakes: 7 }
+store.dispatch(buyCake()); //Updated state { cake: { numOfCakes: 9 }, icecream: { numOfIcecream: 20 } }
+store.dispatch(buyCake()); //Updated state { cake: { numOfCakes: 8 }, icecream: { numOfIcecream: 20 } }
+store.dispatch(buyCake()); //Updated state { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 20 } }
+
+store.dispatch(buyIcecream()); //Updated state { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 19 } }
+store.dispatch(buyIcecream()); //Updated state { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 18 } }
 
 unsubscribe();
